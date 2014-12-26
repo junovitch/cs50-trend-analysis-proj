61ff944050e914d4ba222cf0d1157a0df5f47ce8
Jason Unovitch <jason.unovitch@gmail.com>
2014-12-25 22:56:12 -0500

    Subroutine-ify parsing functionality, move general logic above all subroutines, fix style nits

--- final-project/parse.pl
+++ final-project/parse.pl
@@ -22,13 +22,7 @@ my $DATE_FORMAT = "YYYYMMDD"; # YYYYQQ by default, valid values are listed below
 my @VALID_DATE_FORMATS = ("YYYYMMDD", "YYYYMM", "YYYYQQ", "YYYY");
 
 ################################################################################
-# Variable Declarations
-################################################################################
-my %clients;
-my @uncatagorized;
-
-################################################################################
-# Pre-Execution Checks
+# Pre-Execution Bail Checks
 ################################################################################
 
 die "No files specified.\n" unless @ARGV;
@@ -37,192 +31,22 @@ my %DATE_FORMAT_CHECK = map { $_ => 1 } @VALID_DATE_FORMATS;
 die "Invalid Date Format Specified: \"$DATE_FORMAT\" not valid" unless exists($DATE_FORMAT_CHECK{$DATE_FORMAT});
 
 ################################################################################
+# Variable Declarations
+################################################################################
+my %clients;
+my @uncatagorized;
+
+################################################################################
 # Main Program
 ################################################################################
+
+# Iterate through each input file and call parse_input_file subroutine
 foreach my $arg (@ARGV)
 {
 	my @files = glob $arg;
 	foreach my $file (@files)
 	{
-		open(FH, "<", $file) or die "Unable to open $file";
-		while (<FH>)
-		{
-			# Strip newline and extra spaces
-			chomp; s/\s+/ /go;
-
-			# Support NCSA extended/combined log format described by
-			# http://httpd.apache.org/docs/2.2/mod/mod_log_config.html
-			my ($clientAddress,     $identdLogname, $username,
-			    $localTime,         $httpRequest,   $statusCode,
-			    $bytesSentToClient, $referer,       $userAgent) =
-			    /^(\S+) (\S+) (\S+) \[(.+)\] \"(.+)\" (\S+) (\S+) \"(.*?)\" \"(.*?)\"/o;
-
-
-			my $date;
-			if ($localTime =~ /^(\d{1,2})\/(\w{3})\/(\d{4})/i)
-			{
-				# Fill temporary variables based off each position pattern matched above
-				my ($day, $month, $year) = ($1, $2, $3);
-
-				# For all cases, set starting date. For YYYY case, this is it.
-				$date = "$year";
-
-				# For the by quarter-YYYYQQ case, append the quarter based on month
-				if ($DATE_FORMAT =~ /^YYYYQQ$/)
-				{
-					for ($month)
-					{
-						if    (/JAN/i) { $date .= "Q1"; }
-						elsif (/FEB/i) { $date .= "Q1"; }
-						elsif (/MAR/i) { $date .= "Q1"; }
-						elsif (/APR/i) { $date .= "Q2"; }
-						elsif (/MAY/i) { $date .= "Q2"; }
-						elsif (/JUN/i) { $date .= "Q2"; }
-						elsif (/JUL/i) { $date .= "Q3"; }
-						elsif (/AUG/i) { $date .= "Q3"; }
-						elsif (/SEP/i) { $date .= "Q3"; }
-						elsif (/OCT/i) { $date .= "Q4"; }
-						elsif (/NOV/i) { $date .= "Q4"; }
-						elsif (/DEC/i) { $date .= "Q4"; }
-					}
-				}
-
-				# For the by month cases, append the numeric value of the month
-				if ($DATE_FORMAT =~ /^YYYYMM$/ || $DATE_FORMAT =~ /^YYYYMMDD$/)
-				{
-					for ($month)
-					{
-						if    (/JAN/i) { $date .= "01"; }
-						elsif (/FEB/i) { $date .= "02"; }
-						elsif (/MAR/i) { $date .= "03"; }
-						elsif (/APR/i) { $date .= "04"; }
-						elsif (/MAY/i) { $date .= "05"; }
-						elsif (/JUN/i) { $date .= "06"; }
-						elsif (/JUL/i) { $date .= "07"; }
-						elsif (/AUG/i) { $date .= "08"; }
-						elsif (/SEP/i) { $date .= "09"; }
-						elsif (/OCT/i) { $date .= "10"; }
-						elsif (/NOV/i) { $date .= "11"; }
-						elsif (/DEC/i) { $date .= "12"; }
-					}
-				}
-
-				# For the full date string case, also append the two digit numeric day
-				if ($DATE_FORMAT =~ /^YYYYMMDD$/)
-				{
-					$date .= sprintf "%02d", $day;
-				}
-			}
-			else
-			{
-				# If unable to pattern match date, set to epoch start as a fallback
-				$date = "19700101";
-			}
-
-			# Figure out which OS to tag as and normalize name
-			for ($userAgent)
-			{
-				if    (/Windows NT (\d\.\d)/) {
-					$clients{$clientAddress}->{"os"}->{"Windows NT $1"}->{$date} += 1;
-				}
-				elsif (/Windows XP/) {
-					$clients{$clientAddress}->{"os"}->{"Windows NT 5.1"}->{$date} += 1;
-				}
-				elsif (/Windows NT;/) {
-					$clients{$clientAddress}->{"os"}->{"Windows NT"}->{$date} += 1;
-				}
-				elsif (/Windows CE;/) {
-					$clients{$clientAddress}->{"os"}->{"Windows CE"}->{$date} += 1;
-				}
-				elsif (/Windows 2000/) {
-					$clients{$clientAddress}->{"os"}->{"Windows 2000"}->{$date} += 1;
-				}
-				elsif (/Windows 98/) {
-					$clients{$clientAddress}->{"os"}->{"Windows 98"}->{$date} += 1;
-				}
-				elsif (/Windows 95/) {
-					$clients{$clientAddress}->{"os"}->{"Windows 95"}->{$date} += 1;
-				}
-				elsif (/Windows (\d\.\d)/) {
-					$clients{$clientAddress}->{"os"}->{"Windows $1"}->{$date} += 1;
-				}
-				elsif (/Windows Phone (\d\.\d);/) {
-					$clients{$clientAddress}->{"os"}->{"Windows Phone $1"}->{$date} += 1;
-				}
-				elsif (/Android;/) {
-					$clients{$clientAddress}->{"os"}->{"Android"}->{$date} += 1;
-				}
-				elsif (/Android\/(\d*?\.\d*?);/ || /Android\/(\d*?\.\d*?\.\d*?) /) {
-					$clients{$clientAddress}->{"os"}->{"Android $1"}->{$date} += 1;
-				}
-				elsif (/iPad;/) {
-					$clients{$clientAddress}->{"os"}->{"iPad"}->{$date} += 1;
-				}
-				elsif (/iPhone;/) {
-					$clients{$clientAddress}->{"os"}->{"iPhone"}->{$date} += 1;
-				}
-				elsif (/X11;/ || /Linux;/) {
-					$clients{$clientAddress}->{"os"}->{"Linux"}->{$date} += 1;
-				}
-				elsif (/Mac OS X (\d*?)\.(\d*?);/ || /Mac OS X (\d*?)_(\d*?)/) {
-					$clients{$clientAddress}->{"os"}->{"Mac OS X $1.$2"}->{$date} += 1;
-				}
-				elsif (/bingbot/ || /Baiduspider/ || /MJ12bot/ || /SeznamBot/ ||
-				      /CCBot/ || /Googlebot/ || /YandexBot/ || /Cliqzbot/ ||
-				      /Yahoo! Slurp/ || /archive\.org_bot/ || /NerdyBot/ ||
-				      /AhrefsBot/ || /Sogou web spider/ || /sukibot_heritrix/ ||
-				      /MediaBot/ || /Mail\.RU_Bot/ || /DotBot/ || /Exabot/ ||
-				      /DomainTunoCrawler/ || /rogerbot/ || /memoryBot/ ||
-				      /Microsoft-WebDAV-MiniRedir/ || /AdsBot-Google/) {
-					next; # ignore these
-				}
-				else {
-					push @uncatagorized, $userAgent;
-				}
-			}
-
-			# Figure out which browser to tag as and normalize name
-			for ($userAgent)
-			{
-				if    (/MSIE (\d*?)\./) {
-					$clients{$clientAddress}->{"browser"}->{"MSIE $1"}->{$date} += 1;
-				}
-				elsif ((/rv:11/) && (/Trident\/7.0/)) {
-					$clients{$clientAddress}->{"browser"}->{"MSIE 11"}->{$date} += 1;
-				}
-				elsif (/Microsoft Internet Explorer\/(\d*?)\./) {
-					$clients{$clientAddress}->{"browser"}->{"MSIE $1"}->{$date} += 1;
-				}
-				elsif (/Chrome\/(\d*?)\./) {
-					$clients{$clientAddress}->{"browser"}->{"Chrome $1"}->{$date} += 1;
-				}
-				elsif (/Firefox\/(\d*?)\./) {
-					$clients{$clientAddress}->{"browser"}->{"Firefox $1"}->{$date} += 1;
-				}
-				elsif (/Opera\/(\d*?)\./) {
-					$clients{$clientAddress}->{"browser"}->{"Opera $1"}->{$date} += 1;
-				}
-				elsif (/Opera (\d*?)\./) {
-					$clients{$clientAddress}->{"browser"}->{"Opera $1"}->{$date} += 1;
-				}
-				elsif (/Safari\/(\d*?)\./) {
-					$clients{$clientAddress}->{"browser"}->{"Safari $1"}->{$date} += 1;
-				}
-				elsif (/bingbot/ || /Baiduspider/ || /MJ12bot/ || /SeznamBot/ ||
-				      /CCBot/ || /Googlebot/ || /YandexBot/ || /Cliqzbot/ ||
-				      /Yahoo! Slurp/ || /archive\.org_bot/ || /NerdyBot/ ||
-				      /AhrefsBot/ || /Sogou web spider/ || /sukibot_heritrix/ ||
-				      /MediaBot/ || /Mail\.RU_Bot/ || /DotBot/ || /Exabot/ ||
-				      /DomainTunoCrawler/ || /rogerbot/ || /memoryBot/ ||
-				      /Microsoft-WebDAV-MiniRedir/ || /AdsBot-Google/) {
-					next; # ignore these
-				}
-				else {
-					push @uncatagorized, $userAgent;
-				}
-			}
-		}
-		close(FH);
+		parse_input_file("$file");
 	}
 }
 
@@ -234,15 +58,207 @@ Hash::Util::lock_hash_recurse(%clients);
 if ($DEBUG) { debug_dump_hash("DEBUG.txt", \%clients); }
 if ($DEBUG) { debug_dump_array("UNCATAGORIZED.txt", \@uncatagorized); }
 
+# Call sort_trends subroutine to extract usable info out of our data
 sort_trends('Distribution By OS', 'os', 'osHash.txt');
 sort_trends('Distribution By Browser', 'browser', 'browserHash.txt');
+
+################################################################################
+# Subroutines
+################################################################################
+
+sub parse_input_file
+{
+	# Quit unless correct number of arguments passed, else assign them
+	die "ERROR: Wrong number of arguments in subroutine" unless @_ == 1;
+	my $file = $_[0];
+
+	open(my $fh, "<", $file) or die "Unable to open $file";
+	while (<$fh>)
+	{
+		# Strip newline and extra spaces
+		chomp; s/\s+/ /go;
+
+		# Support NCSA extended/combined log format described by
+		# http://httpd.apache.org/docs/2.2/mod/mod_log_config.html
+		my ($clientAddress,     $identdLogname, $username,
+		    $localTime,         $httpRequest,   $statusCode,
+		    $bytesSentToClient, $referer,       $userAgent) =
+		    /^(\S+) (\S+) (\S+) \[(.+)\] \"(.+)\" (\S+) (\S+) \"(.*?)\" \"(.*?)\"/o;
+
+		# Create $date variable and extract date based on $DATE_FORMAT
+		my $date;
+		if ($localTime =~ /^(\d{1,2})\/(\w{3})\/(\d{4})/i)
+		{
+			# Fill temporary variables based off each position pattern matched above
+			my ($day, $month, $year) = ($1, $2, $3);
+
+			# For all cases, set starting date. For YYYY case, this is it.
+			$date = "$year";
+
+			# For the by quarter-YYYYQQ case, append the quarter based on month
+			if ($DATE_FORMAT =~ /^YYYYQQ$/)
+			{
+				for ($month)
+				{
+					if    (/JAN/i) { $date .= "Q1"; }
+					elsif (/FEB/i) { $date .= "Q1"; }
+					elsif (/MAR/i) { $date .= "Q1"; }
+					elsif (/APR/i) { $date .= "Q2"; }
+					elsif (/MAY/i) { $date .= "Q2"; }
+					elsif (/JUN/i) { $date .= "Q2"; }
+					elsif (/JUL/i) { $date .= "Q3"; }
+					elsif (/AUG/i) { $date .= "Q3"; }
+					elsif (/SEP/i) { $date .= "Q3"; }
+					elsif (/OCT/i) { $date .= "Q4"; }
+					elsif (/NOV/i) { $date .= "Q4"; }
+					elsif (/DEC/i) { $date .= "Q4"; }
+				}
+			}
+
+			# For the by month cases, append the numeric value of the month
+			if ($DATE_FORMAT =~ /^YYYYMM$/ || $DATE_FORMAT =~ /^YYYYMMDD$/)
+			{
+				for ($month)
+				{
+					if    (/JAN/i) { $date .= "01"; }
+					elsif (/FEB/i) { $date .= "02"; }
+					elsif (/MAR/i) { $date .= "03"; }
+					elsif (/APR/i) { $date .= "04"; }
+					elsif (/MAY/i) { $date .= "05"; }
+					elsif (/JUN/i) { $date .= "06"; }
+					elsif (/JUL/i) { $date .= "07"; }
+					elsif (/AUG/i) { $date .= "08"; }
+					elsif (/SEP/i) { $date .= "09"; }
+					elsif (/OCT/i) { $date .= "10"; }
+					elsif (/NOV/i) { $date .= "11"; }
+					elsif (/DEC/i) { $date .= "12"; }
+					}
+			}
+
+			# For the full date string case, also append the two digit numeric day
+			if ($DATE_FORMAT =~ /^YYYYMMDD$/)
+			{
+				$date .= sprintf("%02d", $day);
+			}
+		}
+		else
+		{
+			# If unable to pattern match date, set to epoch start as a fallback
+			$date = "19700101";
+		}
+
+		# Figure out which OS to tag as and normalize name
+		for ($userAgent)
+		{
+			if    (/Windows NT (\d\.\d)/) {
+				$clients{$clientAddress}->{"os"}->{"Windows NT $1"}->{$date} += 1;
+			}
+			elsif (/Windows XP/) {
+				$clients{$clientAddress}->{"os"}->{"Windows NT 5.1"}->{$date} += 1;
+			}
+			elsif (/Windows NT;/) {
+				$clients{$clientAddress}->{"os"}->{"Windows NT"}->{$date} += 1;
+			}
+			elsif (/Windows CE;/) {
+				$clients{$clientAddress}->{"os"}->{"Windows CE"}->{$date} += 1;
+			}
+			elsif (/Windows 2000/) {
+				$clients{$clientAddress}->{"os"}->{"Windows 2000"}->{$date} += 1;
+			}
+			elsif (/Windows 98/) {
+				$clients{$clientAddress}->{"os"}->{"Windows 98"}->{$date} += 1;
+			}
+			elsif (/Windows 95/) {
+				$clients{$clientAddress}->{"os"}->{"Windows 95"}->{$date} += 1;
+			}
+			elsif (/Windows (\d\.\d)/) {
+				$clients{$clientAddress}->{"os"}->{"Windows $1"}->{$date} += 1;
+			}
+			elsif (/Windows Phone (\d\.\d);/) {
+				$clients{$clientAddress}->{"os"}->{"Windows Phone $1"}->{$date} += 1;
+			}
+			elsif (/Android;/) {
+				$clients{$clientAddress}->{"os"}->{"Android"}->{$date} += 1;
+			}
+			elsif (/Android\/(\d*?\.\d*?);/ || /Android\/(\d*?\.\d*?\.\d*?) /) {
+				$clients{$clientAddress}->{"os"}->{"Android $1"}->{$date} += 1;
+			}
+			elsif (/iPad;/) {
+				$clients{$clientAddress}->{"os"}->{"iPad"}->{$date} += 1;
+			}
+			elsif (/iPhone;/) {
+				$clients{$clientAddress}->{"os"}->{"iPhone"}->{$date} += 1;
+			}
+			elsif (/X11;/ || /Linux;/) {
+				$clients{$clientAddress}->{"os"}->{"Linux"}->{$date} += 1;
+			}
+			elsif (/Mac OS X (\d*?)\.(\d*?);/ || /Mac OS X (\d*?)_(\d*?)/) {
+				$clients{$clientAddress}->{"os"}->{"Mac OS X $1.$2"}->{$date} += 1;
+			}
+			elsif (/bingbot/ || /Baiduspider/ || /MJ12bot/ || /SeznamBot/ ||
+			      /CCBot/ || /Googlebot/ || /YandexBot/ || /Cliqzbot/ ||
+			      /Yahoo! Slurp/ || /archive\.org_bot/ || /NerdyBot/ ||
+			      /AhrefsBot/ || /Sogou web spider/ || /sukibot_heritrix/ ||
+			      /MediaBot/ || /Mail\.RU_Bot/ || /DotBot/ || /Exabot/ ||
+			      /DomainTunoCrawler/ || /rogerbot/ || /memoryBot/ ||
+			      /Microsoft-WebDAV-MiniRedir/ || /AdsBot-Google/) {
+				next; # ignore these
+			}
+			else {
+				push @uncatagorized, $userAgent;
+			}
+		}
+
+		# Figure out which browser to tag as and normalize name
+		for ($userAgent)
+		{
+			if    (/MSIE (\d*?)\./) {
+				$clients{$clientAddress}->{"browser"}->{"MSIE $1"}->{$date} += 1;
+			}
+			elsif ((/rv:11/) && (/Trident\/7.0/)) {
+				$clients{$clientAddress}->{"browser"}->{"MSIE 11"}->{$date} += 1;
+			}
+			elsif (/Microsoft Internet Explorer\/(\d*?)\./) {
+				$clients{$clientAddress}->{"browser"}->{"MSIE $1"}->{$date} += 1;
+			}
+			elsif (/Chrome\/(\d*?)\./) {
+				$clients{$clientAddress}->{"browser"}->{"Chrome $1"}->{$date} += 1;
+			}
+			elsif (/Firefox\/(\d*?)\./) {
+				$clients{$clientAddress}->{"browser"}->{"Firefox $1"}->{$date} += 1;
+			}
+			elsif (/Opera\/(\d*?)\./) {
+				$clients{$clientAddress}->{"browser"}->{"Opera $1"}->{$date} += 1;
+			}
+			elsif (/Opera (\d*?)\./) {
+				$clients{$clientAddress}->{"browser"}->{"Opera $1"}->{$date} += 1;
+			}
+			elsif (/Safari\/(\d*?)\./) {
+				$clients{$clientAddress}->{"browser"}->{"Safari $1"}->{$date} += 1;
+			}
+			elsif (/bingbot/ || /Baiduspider/ || /MJ12bot/ || /SeznamBot/ ||
+			      /CCBot/ || /Googlebot/ || /YandexBot/ || /Cliqzbot/ ||
+			      /Yahoo! Slurp/ || /archive\.org_bot/ || /NerdyBot/ ||
+			      /AhrefsBot/ || /Sogou web spider/ || /sukibot_heritrix/ ||
+			      /MediaBot/ || /Mail\.RU_Bot/ || /DotBot/ || /Exabot/ ||
+			      /DomainTunoCrawler/ || /rogerbot/ || /memoryBot/ ||
+			      /Microsoft-WebDAV-MiniRedir/ || /AdsBot-Google/) {
+				next; # ignore these
+			}
+			else {
+				push @uncatagorized, $userAgent;
+			}
+		}
+	}
+	close($fh);
+}
 
 sub sort_trends
 {
 	# Quit unless correct number of arguments passed, else assign them
 	die "ERROR: Wrong number of arguments in subroutine" unless @_ == 3;
-	my $title = $_[0];
-	my $field = $_[1];
+	my $title    = $_[0];
+	my $field    = $_[1];
 	my $dumpfile = $_[2];
 
 	# Allocate temporary variables
@@ -296,12 +312,12 @@ sub print_trends
 {
 	# Quit unless correct number of arguments passed, else assign them
 	die "ERROR: Wrong number of arguments in subroutine" unless @_ == 2;
-	my $title = $_[0];
+	my $title     = $_[0];
 	my %refToHash = %{$_[1]};
 
 	# Allocate temporary variables
 	my $lftLongestKey = 0;
-	my $rgtMaxValue = 0;
+	my $rgtMaxValue   = 0;
 	my $ctrUsableCols;
 	my $divisor;
 
@@ -309,11 +325,11 @@ sub print_trends
 	while ( my ($key, $value) = each %refToHash )
 	{
 		$lftLongestKey = length($key) unless length($key) < $lftLongestKey;
-		$rgtMaxValue = $value unless $value < $rgtMaxValue;
+		$rgtMaxValue   = $value unless $value < $rgtMaxValue;
 	}
 
 	# Calc columns by subtracting TERM_WIDTH w/longest key, 3 (printf whitespace), and longest value
-	$ctrUsableCols = $TERM_WIDTH - $lftLongestKey - 3;
+	$ctrUsableCols  = $TERM_WIDTH - $lftLongestKey - 3;
 	$ctrUsableCols -= length($rgtMaxValue) >= length("COUNT") ? length($rgtMaxValue) : length("COUNT");
 
 	# Calc integer divisor based off max value divided by usable columns
